f7f450a3189780b34d95af2b15371c0ea905894f
Lucas W K <59176579+lucaswkuipers@users.noreply.github.com>
2021-04-12 19:10:07 -0300

    Add files via upload

---
+++ LucasMod/Classes/ViewModel/MyComponent.swift
@@ -0,0 +1,15 @@
+
+public enum MyComponentEnum: String {
+    case Semana = "www.google.com.br"
+    case Dia
+    case Tudo
+}
+
+public struct MinhasConstantes {
+    public static let meuNome = "Galdinho"
+    public static let minhaIdade = 42
+}
+
+public class Compxxx: UIView {
+    var teste: String = ""
+}
